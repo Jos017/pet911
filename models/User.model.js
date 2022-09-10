93cc57f4b31d1c20a9131325d991f34ef6655d39
xavy92 <javaz_15@hotmail.com>
2022-09-09 19:53:33 -0500

    modify user.model

--- models/User.model.js
+++ models/User.model.js
@@ -5,9 +5,30 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      trim: true,
+      required: [true, 'Username is required.'],
+      unique: true
     },
-    password: String,
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.'],
+      match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 'Please use a valid password']
+    },
+    phoneNomber: String,
+    address: String,
+    // petNomber: {{}}   
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
